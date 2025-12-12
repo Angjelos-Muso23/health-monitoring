004bec1223a5d07809b69d6354ad6ed9c18b87d3
Angjelos Muso <angjelos.muso@lhind.dlh.de>
2025-12-12 13:25:10 +0100

    first commit

---
+++ health-monitoring/src/test/java/com/smarthealth/health_monitoring/HealthMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealth.health_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
